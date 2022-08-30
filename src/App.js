29fbcb4e582639fa3df2905b07762d467afe49e4
geworgiean <armangevorgyan@protonmail.com>
2022-08-30 20:22:35 +0400

    React & Firebase

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] `
+}
+
+function App() {
+  return (
+    <div className={style.bg}>
+      
+    </div>
+  );
+}
+
+export default App;
